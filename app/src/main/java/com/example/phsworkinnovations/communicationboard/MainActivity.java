e2617e5e9e13ec087d2a6eea0b89e92acc3b217a
James Jackson <jamesdanjackson@gmail.com>
2016-12-14 11:08:31 -0500

    Version 2 - started textToSpeech

--- app/src/main/java/com/example/phsworkinnovations/communicationboard/MainActivity.java
+++ app/src/main/java/com/example/phsworkinnovations/communicationboard/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.phsworkinnovations.communicationboard;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
 import android.view.*;
-public class MainActivity extends AppCompatActivity {
 
+import java.util.Locale;
+
+import static java.util.Locale.*;
+
+public class MainActivity extends AppCompatActivity {
+    private TextToSpeech speech;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        speech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    speech.setLanguage(Locale.US);
+                }
+            }
+        });
+    }
+    public void playYes(View view){
+        speech.speak("Yes",TextToSpeech.QUEUE_FLUSH, null, null);
     }
 }
