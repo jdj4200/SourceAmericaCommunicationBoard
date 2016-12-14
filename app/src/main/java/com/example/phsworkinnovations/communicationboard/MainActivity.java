e924d3be2b2f804854aa35eb2ea0db148ff34f3f
James Jackson <jamesdanjackson@gmail.com>
2016-12-13 19:19:34 -0500

    Version 1 - yes and no buttons

---
+++ app/src/main/java/com/example/phsworkinnovations/communicationboard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.phsworkinnovations.communicationboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
