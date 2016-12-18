f0b65c31d2a3fc4e0495f7397796dacc16a547bd
James Jackson <jamesdanjackson@gmail.com>
2016-12-17 23:11:30 -0500

    Version 4 - Home page is basically done, Common Expressions page has please and thank you, Topics page has a workplace button,

--- app/src/main/java/com/example/phsworkinnovations/communicationboard/TopicsActivity.java
+++ app/src/main/java/com/example/phsworkinnovations/communicationboard/TopicsActivity.java
@@ -4,21 +4,17 @@ import android.annotation.TargetApi;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.*;
-import android.view.*;
-import android.content.Intent;
+import android.view.View;
+
 import java.util.Locale;
 
-import static java.util.Locale.*;
-
-public class MainActivity extends AppCompatActivity {
+public class TopicsActivity extends AppCompatActivity {
     private TextToSpeech speech;
 
     @Override
-    @TargetApi((21))
-        protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_topics);
         speech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -29,18 +25,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     @TargetApi((21))//Plays speech for all speech buttons using their tags
     public void playSpeech(View view){
         if (!speech.isSpeaking())
-            speech.speak((String) view.getTag(),TextToSpeech.QUEUE_FLUSH, null, null);
-    }
-
-    public void switchToCommonExpressions(View view){
-        Intent toCommonExpressions = new Intent(this, CommonExpressionsActivity.class);
-        startActivity(toCommonExpressions);
-    }
-    public void switchToTopics(View view){
-        Intent toTopics = new Intent(this, TopicsActivity.class);
-        startActivity(toTopics);
+            speech.speak((String) view.getTag(), TextToSpeech.QUEUE_FLUSH, null, null);
     }
 }
